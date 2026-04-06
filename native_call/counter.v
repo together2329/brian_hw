980a3038ec25e1f44e6588c7d167cd08fdd6a2ea
Brian Choi <brian@Brianui-MacBookAir.local>
2026-04-06 22:47:47 +0900

    replace native_call/counter.v (+8/-8 lines)

--- native_call/counter.v
+++ native_call/counter.v
@@ -2,14 +2,14 @@ module counter (
     input  wire        clk,
     input  wire        rst_n,
     input  wire        en,
-    output reg  [7:0]  count
+    output reg  [31:0]  count
 );
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n)
-        count <= 8'd0;
+        count <= 32'd0;
     else if (en)
-        count <= count + 8'd1;
+        count <= count + 32'd1;
 end
 
 endmodule
